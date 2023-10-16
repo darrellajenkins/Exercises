4c62e273aefa64f8eaa11c1aedc27b76b712c033
Darrell Jenkins <142088111+darrellajenkins@users.noreply.github.com>
2023-10-16 00:50:40 -0700

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -1,4 +1,4 @@
-# Main.py
+# main.py
 
 import turtle
 
@@ -66,4 +66,116 @@ while gameon:
 
 win.exitonclick()
 
-#
+# scoreboard.py
+
+import turtle
+
+
+class Scoreboard(turtle.Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.speed(0)
+        self.penup()
+        self.goto(0, 270)
+        self.color("purple")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score:  {self.score}", align="center", font=('Bauhaus 93', 18, 'normal'))
+
+    def gameover(self):
+        self.goto(0, 0)
+        self.color("red")
+        self.write(arg="GAME OVER", align="center", font=('Bauhaus 93', 24, 'normal'))
+
+    def keep_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+# food.py
+
+import turtle
+
+
+import random
+
+
+class Food(turtle.Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed(0)
+        self.refresh()
+
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
+
+# snake.py
+
+import turtle
+
+
+START = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in START:
+            self.addseg(position)
+
+    def addseg(self, position):
+        sly = turtle.Turtle("square")
+        sly.color("white")
+        sly.shapesize(stretch_wid=0.45, stretch_len=0.85)  # added this to make it look more like a snake
+        sly.penup()
+        sly.goto(position)
+        self.segments.append(sly)
+
+    def extend(self):
+        self.addseg(self.segments[-1].position())
+
+    def move(self):
+        for segnum in range(len(self.segments) - 1, 0, -1):
+            newx = self.segments[segnum - 1].xcor()
+            newy = self.segments[segnum - 1].ycor()
+            self.segments[segnum].goto(newx, newy)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+
